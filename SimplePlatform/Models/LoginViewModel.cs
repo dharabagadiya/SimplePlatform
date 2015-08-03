9e3851285a8497e2fec8319922da14c96ed7f2c7
dharabagadiya <dharabagadiya1990@gmail.com>
2015-08-03 20:58:40 +0530

    Change Revert Back

--- SimplePlatform/Models/LoginViewModel.cs
+++ SimplePlatform/Models/LoginViewModel.cs
@@ -10,7 +10,7 @@ namespace Security.Models
     {
         [Required]
         [StringLength(50)]
-        [EmailAddress(ErrorMessage = "A valid email address is required")]
+        [EmailAddress(ErrorMessage = "A valid email address is required.")]
         [Display(Name = "email")]
         public string Username { get; set; }
 
