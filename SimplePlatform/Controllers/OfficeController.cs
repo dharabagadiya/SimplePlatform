74e4336d237fd58fd00c7ded699e4631cf94ee78
dharabagadiya <dhara@9979959417>
2015-08-04 12:10:58 +0530

    Changes Added Office Code Added

---
+++ SimplePlatform/Controllers/OfficeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimplePlatform.Controllers
+{
+    public class OfficeController : Controller
+    {
+        // GET: Office
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Add()
+        {
+            return PartialView();
+        }
+    }
+}
