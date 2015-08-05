c0770229b6ecdc79b9cee72caab8ce2690dee226
dharabagadiya <dharabagadiya1990@gmail.com>
2015-08-06 00:37:32 +0530

    add office validation

--- SimplePlatform/Scripts/Common/common.js
+++ SimplePlatform/Scripts/Common/common.js
@@ -82,6 +82,90 @@ simplePlatform.BindHeaderAddUserClickEvent = function () {
 simplePlatform.BindHeaderAddClickEvents = function () {
     this.BindHeaderAddUserClickEvent();
 };
+
+simplePlatform.ValidateModalOfficeForm = function (obj) {
+    obj.find("form")
+    .bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            name: {
+                message: 'The username is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_]+$/,
+                        message: 'The username can only consist of alphabetical, number and underscore'
+                    }
+                }
+            },
+            contactNo: {
+                message: 'The username is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_]+$/,
+                        message: 'The username can only consist of alphabetical, number and underscore'
+                    }
+                }
+            }
+        }
+    }).on('success.form.bv', function (e) {
+        e.preventDefault();
+        var formObj = $(e.target);;
+        var name = formObj.find("#txtName").val();
+        var contactNo = formObj.find("#txtContactNo").val();
+        var city = formObj.find("#txtCity").val();
+        $.ajax({
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            type: "POST",
+            url: "/Office/Add",
+            async: false,
+            data: JSON.stringify({ "name": name, "contactNo": contactNo, "city": city }),
+            success: function (data) {
+                var status = data;
+                if (status) {
+                    obj.modal('hide');
+                } else {
+                    obj.find("#divCommonMessage").removeClass("hidden");
+                }
+            }
+        });
+    });
+};
+simplePlatform.BindHeaderAddOfficeClickEvent = function () {
+    var obj = $("#lnkAddOffice");
+    obj.off("click.lnkAddOffice").on("click.lnkAddOffice", $.proxy(function (event) {
+        var currentObj = $(event.currentTarget);
+        ShowDialogBox($("#divCommonModalPlaceHolder"), currentObj.attr("url"), null, $.proxy(function (event, dialogContentPlaceHolder) {
+            this.ValidateModalOfficeForm(dialogContentPlaceHolder);
+            dialogContentPlaceHolder.find("#divCommonMessage").addClass("hidden");
+        }, this));
+        return false;
+    }, this));
+};
+simplePlatform.BindHeaderAddOfficeEvents = function () {
+    this.BindHeaderAddOfficeClickEvent();
+};
 $(document).ready(function () {
     simplePlatform.BindHeaderAddClickEvents();
+    simplePlatform.BindHeaderAddOfficeEvents();
 });
