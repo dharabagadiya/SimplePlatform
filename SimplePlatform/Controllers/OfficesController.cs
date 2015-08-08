3ac9be28f0b4f845b7ba80ee6d287febff0af9ac
dharabagadiya <dharabagadiya1990@gmail.com>
2015-08-08 16:10:59 +0530

    Office Commit

---
+++ SimplePlatform/Controllers/OfficesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataModel;
+
+namespace SimplePlatform.Controllers
+{
+    public class OfficesController : BaseController
+    {
+        // GET: Office
+        public ActionResult Index()
+        {
+            BundleConfig.AddStyle("/Offices", "Offices.css", ControllerName);
+            return View();
+        }
+
+        public PartialViewResult Add()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public JsonResult Add(string name, string contactNo, string city)
+        {
+            try
+            {
+                DataContext Context = new DataContext();
+                Context.Offices.Add(new Office
+                {
+                    Name = name,
+                    ContactNo = contactNo,
+                    City = city
+                });
+                var status = Context.SaveChanges();
+                return Json(true);
+            }
+            catch {
+                return Json(false);
+            }
+        }
+    }
+}
