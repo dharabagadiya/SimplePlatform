4424e602161edb61c8c1c6b069df358660ecd1cc
dharabagadiya <dharabagadiya1990@gmail.com>
2015-08-08 16:14:50 +0530

    Office Added

---
+++ SimplePlatform/Scripts/Offices/Offices.js
@@ -0,0 +1,9 @@
+﻿var data = {
+    "Id": 1,
+    "Name": "NGO",
+    "Fundraising": { "Total": 10 },
+    "Task": { "Total": 100 },
+    "Events": { "Total": 100 },
+    "BookingInProcess": { "Total": 100 },
+    "BookingConfirm": { "Total": 100 }
+}
