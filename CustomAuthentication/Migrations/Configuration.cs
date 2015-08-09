48cf097889327e0449e820ac23b71c5900eb832b
mehultechnorise <mehul.chandroliya@gmail.com>
2015-08-09 14:34:18 +0530

    Changes Commited.

--- CustomAuthentication/Migrations/Configuration.cs
+++ CustomAuthentication/Migrations/Configuration.cs
@@ -20,12 +20,13 @@ namespace CustomAuthentication.Migrations
             userRoles.Add(new Role { RoleName = "Admin" });
             userRoles.Add(new Role { RoleName = "Offices FSMS" });
             userRoles.Add(new Role { RoleName = "Speakers" });
-            userRoles.AddRange(userRoles);
+            context.Roles.AddRange(userRoles);
             // Admin Default User Created --- Please Dnt Delete
             var user = new User { UserName = "admin", Email = "admin@gmail.com", FirstName = "Admin", Password = "123456", IsActive = true, CreateDate = DateTime.UtcNow, Roles = new List<Role>() };
             user.Roles.Add(userRoles.Find(model => model.RoleName.Equals("Admin", StringComparison.InvariantCultureIgnoreCase)));
             if (context.Users.Any(model => model.UserName == user.UserName)) { return; }
             context.Users.Add(user);
+            context.SaveChanges();
         }
     }
 }
