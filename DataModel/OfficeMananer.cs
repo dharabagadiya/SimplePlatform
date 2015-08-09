4d7ff2a81fed154db90a43c753a2f998e46fc164
dharabagadiya <dharabagadiya1990@gmail.com>
2015-08-09 18:17:56 +0530

    Office mgr

--- DataModel/OfficeMananer.cs
+++ DataModel/OfficeMananer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -60,5 +61,6 @@ namespace DataModel
         {
             return Context.Offices.Where(modal => modal.IsDeleted == false).ToList();
         }
+       
     }
 }
