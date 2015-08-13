e3d15e9f2eef787f130520ce2a1130a9a8310e92
dharabagadiya <dharabagadiya1990@gmail.com>
2015-08-13 21:55:24 +0530

    event add ui

---
+++ SimplePlatform/Controllers/EventsController.cs
@@ -0,0 +1,40 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimplePlatform.Controllers
+{
+    public class EventsController : BaseController
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            var userDetailManager = new DataModel.UserManager();
+            var officeMananer = new DataModel.OfficeMananer();
+            var user = userDetailManager.GetUserDetail(UserDetail.UserId);
+            var offices = IsAdmin ? officeMananer.GetOffices() : user.Offices.ToList();
+            ViewData["Offices"] = offices;
+            if (IsAdmin)
+            {
+                var customRoleProvider = new CustomAuthentication.CustomRoleProvider();
+                var customMembershipProvider = new CustomAuthentication.CustomMembershipProvider();
+                var role = customRoleProvider.GetRole("Employee");
+                ViewData["Employee"] = customMembershipProvider.GetUsers(role.RoleId);
+            }
+            return PartialView();
+        }
+        [HttpPost]
+        public JsonResult Add(string name, DateTime startDate, DateTime endDate, string description, int officeID)
+        {
+            var eventManager = new EventManager();
+            return Json(eventManager.Add(name, startDate, endDate, description, officeID));
+        }
+    }
+}
--- SimplePlatform/Scripts/Common/common.js
+++ SimplePlatform/Scripts/Common/common.js
@@ -1,4 +1,72 @@
 ﻿var simplePlatform = {};
+simplePlatform.ValidateModalEventForm = function (obj) {
+    obj.find("form")
+        .bootstrapValidator({
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                name: {
+                    message: 'The name is not valid',
+                    validators: {
+                        notEmpty: {
+                            message: 'The name is required and cannot be empty'
+                        },
+                        stringLength: {
+                            min: 5,
+                            max: 30,
+                            message: 'The name must be more than 5 and less than 30 characters long'
+                        },
+                        regexp: {
+                            regexp: /^[a-zA-Z0-9_]+$/,
+                            message: 'The name can contain a-z, A-Z, 0-9, or (_) only'
+                        }
+                    }
+                }
+            }
+        }).on('success.form.bv', function (e) {
+            e.preventDefault();
+            var formObj = $(e.target);;
+            var name = formObj.find("#txtName").val();
+            var startDates = formObj.find("#txtDueDateStart").val();
+            var endDates = formObj.find("#txtDueDateEnd").val();
+            var description = formObj.find("#txtDescription").val();
+            var officeID = formObj.find("#dwnOffices").val();
+            $.ajax({
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                type: "POST",
+                url: "/Events/Add",
+                async: false,
+                data: JSON.stringify({ "name": name, "startDate": startDates, "endDate": endDates, "description": description, "officeID": officeID }),
+                success: function (data) {
+                    var status = data;
+                    if (status) {
+                        obj.modal('hide');
+                        if (!IsNullOrEmpty(office.ReloadOfficeCurrentPageData)) { office.ReloadOfficeCurrentPageData(); }
+                    } else {
+                        obj.find("#divCommonMessage").removeClass("hidden");
+                    }
+                }
+            });
+        });
+};
+simplePlatform.BindHeaderAddEventClickEvent = function () {
+    var obj = $("#lnkAddEvents");
+    obj.off("click.lnkAddEvents").on("click.lnkAddEvents", $.proxy(function (event) {
+        var currentObj = $(event.currentTarget);
+        $("#divCommonModalPlaceHolder").empty();
+        ShowDialogBox($("#divCommonModalPlaceHolder"), currentObj.attr("url"), null, $.proxy(function (event, dialogContentPlaceHolder) {
+            dialogContentPlaceHolder.find("#txtDueDateStart").val(new Date().mmddyyyy());
+            dialogContentPlaceHolder.find("#txtDueDateEnd").val(new Date().mmddyyyy());
+            dialogContentPlaceHolder.find('#datepicker').datepicker({ autoclose: true, todayHighlight: true });
+            this.ValidateModalTaskForm(dialogContentPlaceHolder);
+        }, this));
+        return false;
+    }, this));
+};
 simplePlatform.ValidateModalTaskForm = function (obj) {
     obj.find("form")
         .bootstrapValidator({
@@ -262,6 +330,7 @@ simplePlatform.BindHeaderAddClickEvents = function () {
     this.BindHeaderAddUserClickEvent();
     this.BindHeaderAddOfficeClickEvent();
     this.BindHeaderAddTaskClickEvent();
+    this.BindHeaderAddEventClickEvent();
 };
 $(document).ready(function () {
     simplePlatform.BindHeaderAddClickEvents();
