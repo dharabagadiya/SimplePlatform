cc42c5d660d41da6f1b5c1090ee6f241d529a4de
mehultechnorise <mehul.chandroliya@gmail.com>
2015-08-19 08:31:59 +0530

    Change List: 1) Office Targets Page Added

---
+++ SimplePlatform/Controllers/TargetsController.cs
@@ -0,0 +1,23 @@
+﻿
+#region Using Regions
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+#endregion
+
+namespace SimplePlatform.Controllers
+{
+    public class TargetsController : BaseController
+    {
+        // GET: Targets
+        public ActionResult Index()
+        {
+            var officeManager = new DataModel.OfficeMananer();
+            ViewData["Offices"] = officeManager.GetOffices();
+
+            return View();
+        }
+    }
+}
