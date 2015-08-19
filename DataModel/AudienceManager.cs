793ddc86a6a43cd0c22726b11113399a8167e6e9
mehultechnorise <mehul.chandroliya@gmail.com>
2015-08-20 00:29:51 +0530

    Change List: 1) Audience Code Done

--- DataModel/AudienceManager.cs
+++ DataModel/AudienceManager.cs
@@ -56,6 +56,62 @@ namespace DataModel
             }
         }
 
-        public List<Modal.Audience> GetAudiences() { return Context.Audiences.ToList(); }
+        public bool Update(int audienceID, string name, string contact, DateTime visitDate, int visitTypeID, int officeID, int eventID, int fsmID, int conventionID, bool isBooked, float GSBAmount, float amount)
+        {
+            try
+            {
+                Modal.Office office = null;
+                Modal.Event eventDetail = null;
+                Modal.UserDetail userDetail = null;
+                Modal.Convention convention = null;
+                var visitType = Context.VisitTypes.Where(model => model.VisitTypeId == visitTypeID).FirstOrDefault();
+                office = Context.Offices.Where(model => model.OfficeId == officeID && model.IsDeleted == false).FirstOrDefault();
+                eventDetail = Context.Events.Where(model => model.EventId == eventID && model.IsDeleted == false).FirstOrDefault();
+                userDetail = Context.UsersDetail.Where(model => model.UserId == fsmID && model.User.IsDeleted == false).FirstOrDefault();
+                convention = Context.Conventions.Where(model => model.ConventionId == conventionID && model.IsDeleted == false).FirstOrDefault();
+                var audience = GetAudience(audienceID);
+                if (audience == null) { return false; }
+                audience.Name = name;
+                audience.Contact = contact;
+                audience.VisitDate = visitDate;
+                audience.VisitType = visitType;
+                audience.Office = office;
+                audience.Event = eventDetail;
+                audience.UserDetail = userDetail;
+                audience.Convention = convention;
+                audience.GSBAmount = GSBAmount;
+                audience.UpdateDate = DateTime.Now;
+                audience.ConvensionBooking.Amount = amount;
+                audience.ConvensionBooking.IsBooked = isBooked;
+                audience.ConvensionBooking.UpdateDate = DateTime.Now;
+                Context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+
+            try
+            {
+                var audience = GetAudience(id);
+                if (audience == null) { return false; }
+                audience.IsDeleted = true;
+                audience.UpdateDate = DateTime.Now;
+                Context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public List<Modal.Audience> GetAudiences() { return Context.Audiences.Where(model => model.IsDeleted == false).ToList(); }
+        public Modal.Audience GetAudience(int id) { return Context.Audiences.Where(model => model.AudienceID == id && model.IsDeleted == false).FirstOrDefault(); }
     }
 }
