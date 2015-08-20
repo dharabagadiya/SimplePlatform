e161bf6a2ca02a1214608a7444ec50022eaf4255
mehultechnorise <mehul.chandroliya@gmail.com>
2015-08-20 08:43:04 +0530

    Change List: 1) Targets Load Grid Code Done

--- SimplePlatform/Controllers/TargetsController.cs
+++ SimplePlatform/Controllers/TargetsController.cs
@@ -16,8 +16,25 @@ namespace SimplePlatform.Controllers
         {
             var officeManager = new DataModel.OfficeMananer();
             ViewData["Offices"] = officeManager.GetOffices();
-
+            BundleConfig.AddScript("~/Scripts/Offices", "targets.js", ControllerName);
             return View();
+        }
+
+        public JsonResult GetTasks()
+        {
+            var targetManager = new DataModel.TargetManager();
+            var targetList = targetManager.GetTargets();
+            var targets = targetList.Select(model => new
+            {
+                ID = model.TargetId,
+                OfficeName = model.Office.Name,
+                DueDate = model.DueDate,
+                Booking = model.Booking,
+                FundRaising = model.FundRaising,
+                GSB = model.GSB,
+                Arrivals = model.Arrivals
+            }).ToList();
+            return Json(new { data = targets });
         }
     }
 }
---
+++ SimplePlatform/Scripts/Offices/targets.js
@@ -0,0 +1,48 @@
+﻿var targets = {};
+targets.options = {};
+targets.LoadTagetsList = function () {
+    $('#targetList').dataTable({
+        renderer: {
+            "header": "bootstrap",
+            "pageButton": "bootstrap"
+        },
+        "ajax": {
+            "url": "/Targets/GetTasks",
+            "type": "POST"
+        },
+        "displayLength": 25,
+        responsive: true,
+        "deferRender": true,
+        "columns": [
+            { "data": "OfficeName" },
+            { "data": "DueDate" },
+            { "data": "Booking" },
+            { "data": "FundRaising" },
+            { "data": "GSB" },
+            { "data": "Arrivals" },
+            {
+                "data": null,
+                "createdCell": function (cell, cellData, rowData, rowIndex, colIndex) {
+                    var currentObj = $(cell);
+                    currentObj.css({ "text-align": "center" }).data("audience_detail", rowData);
+                    currentObj.off("click.dataTableEditLink").on("click.dataTableEditLink", function () { audiences.EditAudienceDetail($(this)); });
+                },
+                render: function (o) { return '<a href="#"><i class="ui-tooltip fa fa-pencil" style="font-size: 22px;" data-original-title="Edit"></i></a>'; },
+                "orderable": false,
+                "width": '2%'
+            }, {
+                "data": null,
+                "createdCell": function (cell, cellData, rowData, rowIndex, colIndex) {
+                    var currentObj = $(cell);
+                    currentObj.css({ "text-align": "center" }).data("audience_detail", rowData);
+                    currentObj.off("click.dataTableDeleteLink").on("click.dataTableDeleteLink", function () { audiences.DeletAudienceDetail($(this)); });
+                },
+                render: function (o) { return '<a href="#"><i class="ui-tooltip fa fa-trash-o" style="font-size: 22px;" data-original-title="Delete"></i></a>'; },
+                "orderable": false,
+                "width": '2%'
+            }]
+    }).removeClass('display').addClass('table table-striped table-bordered');
+};
+$(document).ready(function () {
+    targets.LoadTagetsList();
+});
