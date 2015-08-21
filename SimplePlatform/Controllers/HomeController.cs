e4e4333f53d705d80559d7350e18576fec83fd26
mehultechnorise <mehul.chandroliya@gmail.com>
2015-08-22 00:53:07 +0530

    Change List: 1) Target List Created

--- SimplePlatform/Controllers/HomeController.cs
+++ SimplePlatform/Controllers/HomeController.cs
@@ -15,7 +15,27 @@ namespace SimplePlatform.Controllers
     {
         public ActionResult Index()
         {
+            BundleConfig.AddScript("~/Scripts/Home", "home.js", ControllerName);
+            StartupScript = "home.DoPageSetting();";
             return View();
+        }
+
+        public JsonResult GetFundRaisingTargets()
+        {
+            var dataSeries = new List<object>();
+            var targetManager = new DataModel.TargetManager();
+            var offices = IsAdmin ? new DataModel.OfficeMananer().GetOffices() : UserDetail.Offices;
+            dataSeries.Add(targetManager.GetFundingTargets(offices.ToList(), DateTime.Now.Year));
+            return Json(dataSeries);
+        }
+
+        public JsonResult GetBookingTargets()
+        {
+            var dataSeries = new List<object>();
+            var targetManager = new DataModel.TargetManager();
+            var offices = IsAdmin ? new DataModel.OfficeMananer().GetOffices() : UserDetail.Offices;
+            dataSeries.Add(targetManager.GetBookingTargets(offices.ToList(), DateTime.Now.Year));
+            return Json(dataSeries);
         }
     }
 }
---
+++ SimplePlatform/Scripts/Home/home.js
@@ -0,0 +1,71 @@
+﻿var home = {};
+home.options = {
+    FundRaisingURl: "/Home/GetFundRaisingTargets",
+    BookingURl: "/Home/GetBookingTargets"
+};
+home.LoadFundRaisingChart = function (data) {
+    var chartArea = $("#divFundRaisingChart");
+    chartArea.highcharts({
+        chart: {},
+        title: { text: "" },
+        subtitle: { text: "" },
+
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: { month: '%b %e, %Y' },
+            title: { text: "Due Date" }
+        },
+        yAxis: {
+            title: { text: 'Target Amount (in $)' }
+        },
+        credits: { enabled: false },
+        tooltip: { headerFormat: '<b>{series.name}</b><br>', pointFormat: '{point.x:%e. %b}: {point.y:.2f} m' },
+        plotOptions: { spline: { marker: { enabled: true } } },
+        series: data
+    });
+};
+home.LoadBookingChart = function (data) {
+    var chartArea = $("#divBookingChart");
+    chartArea.highcharts({
+        chart: {},
+        title: { text: "" },
+        subtitle: { text: "" },
+
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: { month: '%b %e, %Y' },
+            title: { text: "Due Date" }
+        },
+        yAxis: {
+            title: { text: 'No. of Booking' }
+        },
+        credits: { enabled: false },
+        tooltip: { headerFormat: '<b>{series.name}</b><br>', pointFormat: '{point.x:%e. %b}: {point.y:.2f} m' },
+        plotOptions: { spline: { marker: { enabled: true } } },
+        series: data
+    });
+};
+home.GetFundRaisingData = function () {
+    $.ajax({
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        type: "POST",
+        url: home.options.FundRaisingURl,
+        async: true,
+        success: function (data) { home.LoadFundRaisingChart(data); }
+    });
+};
+home.GetBookingData = function () {
+    $.ajax({
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        type: "POST",
+        url: home.options.BookingURl,
+        async: true,
+        success: function (data) { home.LoadBookingChart(data); }
+    });
+};
+home.DoPageSetting = function () {
+    this.GetFundRaisingData();
+    this.GetBookingData();
+};
